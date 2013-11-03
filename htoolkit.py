7ad463357fd383f2fb614843bd553a65cdc1859e
Leonardo da Silva <independent.scientist@gmail.com>
2013-11-03 08:12:48 -0300

    Base commit

--- htoolkit.py
+++ htoolkit.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     # inits torrific
     torrific.initialize('http://192.168.1.123:8118')
 
-    text_data = torrific.get_url_data('http://www.detran.pe.gov.br/')
+    text_data = torrific.get_url_data('http://ifconfig.me/ip')
 
     if text_data == -1:
         exit(-1)
